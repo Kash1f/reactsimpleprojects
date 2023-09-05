f4f8e9158c25463d8ff26ee9d246ef3858d9e100
Kashif <ahmed.kashif1001@gmail.com>
2023-09-06 02:33:06 +0500

    formatting

--- src/App2showtime.js
+++ src/App2showtime.js
@@ -3,26 +3,18 @@ import './App.css'
 
 const App2showtime = () => {
 
-    
     let newTime = new Date().toLocaleTimeString();
-
     const [ctime, setCtime] = useState(newTime);
 
     const UpdateTime = () => {
         let newTime = new Date().toLocaleTimeString();
         setCtime(newTime);
-
     }
 
   return (
-
     <>
    <h1>{ctime}</h1>
-   <button
-        onClick={UpdateTime}>Get Time
-   </button>
-
-
+   <button onClick={UpdateTime}>Get Time</button>
     </>
   )
 }
