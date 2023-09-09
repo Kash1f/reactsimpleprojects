54b00468a77b41694e66fe5a709754c7ed4ac959
Kashif <ahmed.kashif1001@gmail.com>
2023-09-09 22:35:58 +0500

    commitsforsetName

--- src/App.js
+++ src/App.js
@@ -6,22 +6,31 @@ const App = () => {
   const purple = "#8e44ad";
 
   const [bg, setBg] = useState(purple);
+  const [name, setName] = useState('click me');
 
   const bgChange = () => {
   // console.log('clicked');
+
   let newBg = '#34495e'
   setBg(newBg);  //setBg will have newBg variable inside it which makes the curret value of bg=newBg as it is inside the function of setBg which updates the function according to hooks
 
+  setName('Changed');
+  };
 
+  const bgBack = () => {
+    setBg('orange');
+    setName('Damnn');
+
+  };
   
-};
-  return (
+
+return (
     <>
     <div style={{backgroundColor: bg}}>
-       <button onClick={bgChange}>Click Me</button>
+       <button onClick={bgChange} onDoubleClick={bgBack}>{name}</button>
     </div>
     </>
-  )
+)
 }
 
 export default App
